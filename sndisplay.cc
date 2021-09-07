caae1540be938d6703681dced711500ee9f5581c
emchauve <chauveau@cenbg.in2p3.fr>
2021-09-07 12:15:24 +0200

    add demonstrator tracker+calo display

--- sndisplay.cc
+++ sndisplay.cc
@@ -15,7 +15,7 @@ namespace sndisplay
   class calorimeter
   {
   public:
-    calorimeter (const char *n = "") : name (n)
+    calorimeter (const char *n = "") : calorimeter_name (n)
     {
       canvas_it = NULL;
       canvas_fr = NULL;
@@ -259,10 +259,10 @@ namespace sndisplay
     void draw()
     {
       if (canvas_it == NULL)
-	canvas_it = new TCanvas (Form("C_it_%s",name.Data()), name, 900, 600);
+	canvas_it = new TCanvas (Form("C_it_%s",calorimeter_name.Data()), Form("%s (IT side)",calorimeter_name.Data()), 900, 600);
 
       if (canvas_fr == NULL)
-	canvas_fr = new TCanvas (Form("C_fr_%s",name.Data()), name, 900, 600);
+	canvas_fr = new TCanvas (Form("C_fr_%s",calorimeter_name.Data()), Form("%s (FR side)",calorimeter_name.Data()), 900, 600);
 
       if (draw_content)
 	{
@@ -477,7 +477,7 @@ namespace sndisplay
       gSystem->ProcessEvents();
     }
     
-    TString name;
+    TString calorimeter_name;
 
     float range_min, range_max;
 
@@ -503,12 +503,13 @@ namespace sndisplay
     TPad *pad_french;
 
     int palette_index;
-  };
+
+  }; // class calorimeter
 
   class tracker
   {
   public:
-    tracker (const char *n = "") : name (n)
+    tracker (const char *n = "") : tracker_name (n)
     {
       canvas = NULL;
 
@@ -541,8 +542,11 @@ namespace sndisplay
 	    unsigned int cellnum = cell_side*113*9 + cell_row*9 + cell_layer;
 
 	    double x1 = spacerx + cell_row*cell_sizex;
-	    double y1 = spacery + cell_layer*cell_sizey;
-	    if (cell_side == 0) y1 += spacery + 9*cell_sizey;
+	    double y1 = spacery;
+
+	    if (cell_side == 0)
+	      y1 += 9*cell_sizey + spacery + cell_layer*cell_sizey;
+	    else y1 += (8-cell_layer)*cell_sizey;
 
 	    double x2 = x1 + cell_sizex;
 	    double y2 = y1 + cell_sizey;
@@ -613,7 +617,7 @@ namespace sndisplay
     void draw()
     {
       if (canvas == NULL)
-	canvas = new TCanvas (Form("C_%s",name.Data()), name, 1500, 300);
+	canvas = new TCanvas (Form("C_%s",tracker_name.Data()), tracker_name, 1500, 300);
 
       if (draw_content)
 	{
@@ -682,9 +686,9 @@ namespace sndisplay
       content[cellnum] = value;
     }
 
-    void setcontent (int cell_side, int cell_layer, int cell_row, float value)
+    void setcontent (int cell_side, int cell_row, int cell_layer, float value)
     {
-      unsigned int cellnum = cell_side*9*113 + cell_layer*113 + cell_row;
+      unsigned int cellnum = cell_side*9*113 + cell_row*9 + cell_layer;
       if (cellnum < nb_cell) content[cellnum] = value;
       else printf("*** wrong cell ID\n");
     }
@@ -731,7 +735,7 @@ namespace sndisplay
       gSystem->ProcessEvents();
     }
 
-    TString name;
+    TString tracker_name;
 
     float range_min, range_max;
 
@@ -753,8 +757,336 @@ namespace sndisplay
     TCanvas *canvas;
 
     int palette_index;
-  };
-}
+
+  }; // class tracker
+
+
+  class demonstrator
+  {
+  public:
+    demonstrator (const char *n = "") : demonstrator_name (n)
+    {
+      // calorimeter_display = new calorimeter(n);
+      // tracker_display = new tracker(n);
+
+      // TOP_VIEW //
+
+      const double spacerx = 0.0125;
+      const double spacery = 0.0250;
+
+      const double mw_sizey = (1-2*spacery)/(2.0 + 4*1.035 + 0.313);
+      const double xw_sizey = 1.035*mw_sizey;
+      const double se_sizey = 0.313*mw_sizey;
+      const double gg_sizey = (1-2*spacery-2*mw_sizey-se_sizey)/18.0;
+
+      const double mw_sizex = (1-2*spacerx)/(20 + 2*0.5);
+      const double xw_sizex = (1-2*spacerx-20*mw_sizex);
+      const double se_sizex = (1-2*spacerx-2*xw_sizex);
+      const double gg_sizex = se_sizex/113.0;
+
+      // MW (column only)
+
+      for (unsigned int mw_side=0; mw_side<2; ++mw_side) {
+
+	for (unsigned int mw_column=0; mw_column<20; ++mw_column) {
+
+	  double x1 = spacerx + 0.5*xw_sizex + mw_column*mw_sizex;
+	  double y1 = spacery + (1-mw_side)*(mw_sizey+4*xw_sizey+se_sizey);
+
+	  double x2 = x1 + mw_sizex;
+
+	  double y2 = y1 + mw_sizey;
+
+	  top_om_content.push_back(0);
+
+	  TBox *box = new TBox(x1, y1, x2, y2);
+	  box->SetFillColor(0);
+	  box->SetLineWidth(1);
+	  top_om_box.push_back(box);
+	  
+	  TString omid_string = Form("M:%1d.%d.*", mw_side, mw_column);
+	  TText *omid_text = new TText (x1+0.5*mw_sizex, y1+0.667*mw_sizey, omid_string);
+	  omid_text->SetTextSize(0.036);
+	  omid_text->SetTextAlign(22);
+	  top_om_text.push_back(omid_text);
+
+	  // TText *content_text = new TText (x1+0.5*mw_sizex, y1+0.333*mw_sizey, "");
+	  // content_text->SetTextSize(0.02);
+	  // content_text->SetTextAlign(22);
+	  // content_text_v.push_back(content_text);
+
+	} // for mw_column
+	
+      } // for mw_side
+
+      // XW (column only)
+
+      for (unsigned int xw_side=0; xw_side<2; ++xw_side) {
+
+	for (unsigned int xw_wall=0; xw_wall<2; ++xw_wall) {
+
+	  for (unsigned int xw_column=0; xw_column<2; ++xw_column) {
+
+	    unsigned int omnum = 40 + xw_side*2*2 + xw_wall*2 + xw_column;
+
+	    double x1 = spacerx + xw_wall*(xw_sizex+113*gg_sizex);
+	    double x2 = x1 + xw_sizex;
+
+	    double y1 = spacery + mw_sizey;
+
+	    if (xw_side == 0)
+	      y1 += 2*xw_sizey + se_sizey + xw_column*xw_sizey;
+	    else y1 += (1-xw_column)*xw_sizey;
+
+	    double y2 = y1 + xw_sizey;
+
+	    top_om_content.push_back(0);
+
+	    TBox *box = new TBox(x1, y1, x2, y2);
+	    box->SetFillColor(0);
+	    box->SetLineWidth(1);
+	    top_om_box.push_back(box);
+	  
+	    TString omid_string = Form("X:%1d.%1d.%1d.*", xw_side, xw_wall, xw_column);
+	    TText *omid_text = new TText (x1+0.5*mw_sizex, y1+0.6*mw_sizey, omid_string);
+	    omid_text->SetTextSize(0.036);
+	    omid_text->SetTextAlign(22);
+	    top_om_text.push_back(omid_text);
+
+	  }
+	}
+      }
+
+      for (unsigned int gg_side=0; gg_side<2; ++gg_side) {
+
+	  for (unsigned int gg_row=0; gg_row<113; ++gg_row) {
+
+	    for (unsigned int gg_layer=0; gg_layer<9; ++gg_layer) {
+
+	      unsigned int ggnum = gg_side*113*9 + gg_row*9 + gg_layer;
+
+	      double x1 = spacerx + xw_sizex + gg_row*gg_sizex;
+	    
+	      double y1 = spacery + mw_sizey;
+	      if (gg_side == 0)
+		y1 += 9*gg_sizey + se_sizey + gg_layer*gg_sizey;
+	      else 
+		y1 += (8-gg_layer)*gg_sizey;
+
+	      double x2 = x1 + gg_sizex;
+	      double y2 = y1 + gg_sizey;
+
+	      top_gg_content.push_back(0);
+
+	      TBox *box = new TBox(x1, y1, x2, y2);
+	      box->SetFillColor(0);
+	      box->SetLineWidth(1);
+	      top_gg_box.push_back(box);
+
+	      TEllipse *ellipse = new TEllipse((x1+x2)/2, (y1+y2)/2, gg_sizex/2, gg_sizey/2);
+	      ellipse->SetFillColor(0);
+	      ellipse->SetLineWidth(1);
+	      top_gg_ellipse.push_back(ellipse);
+	    }
+	  }
+      }
+
+      const Int_t nRGBs = 6;
+      Double_t stops[nRGBs] = { 0.00, 0.20, 0.40, 0.60, 0.80, 1.00 };
+      Double_t red[nRGBs]   = { 0.25, 0.00, 0.20, 1.00, 1.00, 0.90 };
+      Double_t green[nRGBs] = { 0.25, 0.80, 1.00, 1.00, 0.80, 0.00 };
+      Double_t blue[nRGBs]  = { 1.00, 1.00, 0.20, 0.00, 0.00, 0.00 };
+      
+      palette_index = TColor::CreateGradientColorTable(nRGBs, stops, red, green, blue, 100);
+
+    } // demonstrator ()
+
+    void draw_top()
+    {
+      if (demonstrator_canvas == NULL)
+	demonstrator_canvas = new TCanvas (Form("C_demonstrator_%s",demonstrator_name.Data()), Form("%s",demonstrator_name.Data()), 1800, 450);
+
+      for (unsigned int mw_side=0; mw_side<2; ++mw_side)
+	{
+	  for (unsigned int mw_column=0; mw_column<20; ++mw_column)
+	    {
+	      unsigned int top_om_num = mw_side*20 + mw_column;
+ 	      top_om_box[top_om_num]->Draw("l");
+	      top_om_text[top_om_num]->Draw();
+	    }
+	}
+
+      for (unsigned int xw_side=0; xw_side<2; ++xw_side)
+	{
+	  for (unsigned int xw_wall=0; xw_wall<2; ++xw_wall)
+	    {
+	      for (unsigned int xw_column=0; xw_column<2; ++xw_column)
+		{
+		  unsigned int top_om_num = 40 + xw_side*2*2 + xw_wall*2 + xw_column;
+		  top_om_box[top_om_num]->Draw("l");
+		  top_om_text[top_om_num]->Draw();
+		}
+	    }
+	}
+      
+      for (unsigned int gg_side=0; gg_side<2; ++gg_side)
+	{
+	  for (unsigned int gg_row=0; gg_row<113; ++gg_row)
+	    {
+	      for (unsigned int gg_layer=0; gg_layer<9; ++gg_layer)
+		{
+		  unsigned int top_gg_num = gg_side*113*9 + gg_row*9 + gg_layer;
+		  top_gg_box[top_gg_num]->Draw("l");
+		  top_gg_ellipse[top_gg_num]->Draw("l");
+		  // top_g_text[top_gg_num]->Draw();
+		}
+	    }
+	}
+
+
+    } // draw_top
+
+    void setomcontent (int om_num, float value)
+    {
+      int top_om_num = -1;
+      
+      if (om_num < 260) // MW IT
+	{
+	  unsigned int om_side = 0;
+	  unsigned int om_column = (om_num/13);
+	  top_om_num = om_side*20 + om_column;
+	}
+      else if (om_num < 520) // MW IT
+	{
+	  unsigned int om_side = 1;
+	  unsigned int om_column = (om_num-260)/13;
+	  top_om_num = om_side*20 + om_column;
+	}
+      else if (om_num < 648) // XW
+	{
+	  unsigned int om_side = (om_num < 584) ? 0 : 1;
+	  unsigned int om_wall = (om_num-520-om_side*64)/32;
+	  unsigned int om_column = (om_num-520-om_side*64-om_wall*32)/16;
+	  top_om_num = 40 + om_side*2*2 + om_wall*2 + om_column;
+	}
+
+      top_om_content[top_om_num] = value;
+    }
+
+
+    void setggcontent (int cell_side, int cell_layer, int cell_row, float value)
+    {
+      unsigned int cellnum = cell_side*9*113 + cell_row*9 + cell_layer;
+      if (cellnum < 2034) top_gg_content[cellnum] = value;
+      else printf("*** wrong cell ID\n");
+    }
+
+    void setggcontent (int cell_num, float value)
+    {
+      int cell_side = cell_num/1017;
+      int cell_row = (cell_num-cell_side*1017)/9;
+      int cell_layer = cell_num%9;
+      setggcontent (cell_side, cell_layer, cell_row, value);
+    }
+
+
+    void reset()
+    {
+      for (size_t om=0; om<top_om_content.size(); ++om)
+	{
+	  top_om_content[om] = 0;
+	  top_om_box[om]->SetFillColor(0);
+	}
+
+      for (size_t gg=0; gg<top_gg_content.size(); ++gg)
+	{
+	  top_gg_content[gg] = 0;
+	  top_gg_ellipse[gg]->SetFillColor(0);
+	  // top_gg_box[gg]->SetFillColor(0);
+	}
+
+      demonstrator_canvas->Modified();
+      demonstrator_canvas->Update();
+
+      gSystem->ProcessEvents();
+    }
+
+    void update()
+    {
+      float top_content_min = top_om_content[0];
+      float top_content_max = top_om_content[0];
+
+      for (size_t om=0; om<top_om_content.size(); ++om)
+    	{
+    	  if (top_om_content[om] < top_content_min) top_content_min = top_om_content[om];
+    	  if (top_om_content[om] > top_content_max) top_content_max = top_om_content[om];
+    	}
+
+      for (size_t gg=0; gg<top_gg_content.size(); ++gg)
+    	{
+    	  if (top_gg_content[gg] < top_content_min) top_content_min = top_gg_content[gg];
+    	  if (top_gg_content[gg] > top_content_max) top_content_max = top_gg_content[gg];
+    	}
+
+      top_content_min = 0;
+      // if (range_min != -1) top_content_min = range_min;
+      // if (range_max != -1) top_content_max = range_max;
+
+      for (size_t om=0; om<top_om_content.size(); ++om)
+    	{
+    	  if (top_om_content[om] != 0)
+    	    {
+    	      int color_index = floor (99*(top_om_content[om]-top_content_min)/(top_content_max-top_content_min));
+    	      if (color_index < 0) color_index = 0;
+    	      else if (color_index >= 100) color_index = 99;
+    	      top_om_box[om]->SetFillColor(palette_index + color_index);
+    	    }
+    	  else
+    	    top_om_box[om]->SetFillColor(0);
+    	}
+
+      for (size_t gg=0; gg<top_gg_content.size(); ++gg)
+    	{
+    	  if (top_gg_content[gg] != 0)
+    	    {
+    	      int color_index = floor (99*(top_gg_content[gg]-top_content_min)/(top_content_max-top_content_min));
+    	      if (color_index < 0) color_index = 0;
+    	      else if (color_index >= 100) color_index = 99;
+    	      top_gg_ellipse[gg]->SetFillColor(palette_index + color_index);
+    	    }
+    	  else
+    	    top_gg_ellipse[gg]->SetFillColor(0);
+    	}
+
+      demonstrator_canvas->Modified();
+      demonstrator_canvas->Update();
+
+      gSystem->ProcessEvents();
+    }
+
+    //
+
+    TString demonstrator_name;
+
+    // calorimeter *calorimeter_display;
+    // tracker *tracker_display;
+
+    std::vector<float> top_om_content;
+    std::vector<TBox*> top_om_box;
+    std::vector<TText*> top_om_text;
+
+    std::vector<float> top_gg_content;
+    std::vector<TBox*> top_gg_box;
+    std::vector<TEllipse*> top_gg_ellipse;
+    // std::vector<TText*>  top_gg_text;
+
+    TCanvas *demonstrator_canvas;
+
+    int palette_index;
+
+  }; // class demonstrator
+
+} // namespace sndisplay
 
 // sndisplay_test(): example of sndisplay::calorimeter usage
 
